8bc1b4b20edaa8866c17e05c1766d38617402c5f
Alice Pote <alice.writes.wrongs@gmail.com>
2023-05-19 17:14:58 -0400

    I thought it would be fun to build a bf terminal, so doing that

--- frontend/components/BrainfuckRepl.tsx
+++ frontend/components/BrainfuckRepl.tsx
@@ -1,8 +1,39 @@
 import React, { useCallback, useState } from "react"
+import { createUseStyles } from "react-jss"
+import { BF_HELLO_WORLD } from "../util/constants"
+
+const useStyles = createUseStyles({
+  terminal: {
+    backgroundColor: "black",
+    minHeight: "60em",
+    maxWidth: "80em",
+    color: "white",
+    margin: "auto",
+  },
+  terminalRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  terminalInput: {
+    background: "none",
+    color: "white",
+    border: "none",
+    width: "100%",
+
+    "&:focus-visible": {
+      border: "none",
+    },
+  },
+  terminalCaret: {
+    color: "white",
+  },
+})
 
 export default function BrainfuckEnvironment() {
-  const [bfCode, setBfCode] = useState("")
+  const [bfCode, setBfCode] = useState(BF_HELLO_WORLD)
   const [output, setOutput] = useState("")
+
+  const classes = useStyles()
 
   const runBrainfuck = useCallback(() => {
     if (bfCode !== "") {
@@ -27,9 +58,14 @@ export default function BrainfuckEnvironment() {
       />
       <div>
         <button onClick={runBrainfuck}>run!</button>
-        <h1>output</h1>
         <code>
-          <pre>{output}</pre>
+          <pre className={classes.terminal}>
+            <div className={classes.terminalRow}>
+              <span className={classes.terminalCaret}>{" > "}</span>
+              <input className={classes.terminalInput} />
+            </div>
+            {output}
+          </pre>
         </code>
       </div>
     </div>
